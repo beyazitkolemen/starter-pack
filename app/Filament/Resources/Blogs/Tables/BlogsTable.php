d92037040837ae64090412787f2ad8d64d6ad8a9
Beyazıt Kölemen <bkolemen@gmail.com>
2025-08-25 17:33:26 +0300

    .

---
+++ app/Filament/Resources/Blogs/Tables/BlogsTable.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Filament\Resources\Blogs\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class BlogsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')
+                    ->searchable(),
+                TextColumn::make('slug')
+                    ->searchable(),
+                TextColumn::make('status')
+                    ->searchable(),
+                ImageColumn::make('featured_image'),
+                TextColumn::make('author.name')
+                    ->searchable(),
+                TextColumn::make('category.name')
+                    ->searchable(),
+                TextColumn::make('published_at')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('view_count')
+                    ->numeric()
+                    ->sortable(),
+                IconColumn::make('is_featured')
+                    ->boolean(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Tags/Schemas/TagForm.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Resources\Tags\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Schema;
+
+class TagForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('slug')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+                TextInput::make('color'),
+                Toggle::make('is_active')
+                    ->required(),
+                TextInput::make('usage_count')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+            ]);
+    }
+}
