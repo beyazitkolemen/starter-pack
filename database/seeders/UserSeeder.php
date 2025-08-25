7742fbb6be0323ca5483e956c8e8e2b0bddbbf18
Beyazıt Kölemen <bkolemen@gmail.com>
2025-08-25 15:24:32 +0300

    .

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Beyazıt Kölemen',
+            'email' => 'beyazit@artf4.com',
+            'password' => Hash::make('12345678'),
+            'email_verified_at' => now(),
+        ]);
+
+
+    }
+}
